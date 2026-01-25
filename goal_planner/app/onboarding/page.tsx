a8e41fb3a787b5a33344001c61a333443aa57566
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2026-01-25 04:38:16 -0300

    Update page.tsx

--- goal_planner/app/onboarding/page.tsx
+++ goal_planner/app/onboarding/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { useState } from "react";
+import OnboardingFirstStep from "@/components/Onboarding/OnboardingFirstStep/OnboardingFirstStep";
+
 export default function OnboardingPage() {
-    return <div>Onboarding</div>;
+	const [currentStep, setCurrentStep] = useState(1);
+
+	const handleNext = () => {
+		setCurrentStep(currentStep + 1);
+	};
+
+	const handlePrevious = () => {
+		setCurrentStep(currentStep - 1);
+	};
+
+	return (
+		<div>
+			{currentStep === 1 && <OnboardingFirstStep onNext={handleNext} />}
+			{/* 			{currentStep === 2 && (
+				<OnboardingSecondStep onNext={handleNext} onPrevious={handlePrevious} />
+			)} */}
+		</div>
+	);
 }
